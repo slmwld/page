f0035dbef0139aea19096668cd1eb69cbe9effd9
slmwld02 <slmwld02@gmail.com>
2019-04-01 16:33:03 -0600

    primer commit

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// var logo = document.querySelector('#logo');
+
+// logo.addEventListener('mouseenter', function(e){
+//     console.log('tigered')
+//     this.src='img/logopag_hober.png';
+// });
